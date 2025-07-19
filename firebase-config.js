ff067cdfc843f2a068ea525a4d058c7561e7b574
AbdAllah-Amr-Ali <138628732+AbdAllah-Amr-Ali@users.noreply.github.com>
2025-07-19 22:29:26 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyBgvcqvsY9XoSXVC4TrB0YkUDBfLR26pq0",
+        authDomain: "asg-control-store-cb0b8.firebaseapp.com",
+        projectId: "asg-control-store-cb0b8",
+        storageBucket: "asg-control-store-cb0b8.firebasestorage.app",
+        messagingSenderId: "233708170637",
+        appId: "1:233708170637:web:bd96e7276a1d23bb05a1c4",
+        measurementId: "G-XVYTT16SW7"
+    };
